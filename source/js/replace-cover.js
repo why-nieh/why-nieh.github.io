9beeec5b3424a946979d307a07a3d23dd5622df4
NiehXiaoBin <niehxiaobin@163.com>
2026-07-01 16:16:40 +0800

    update

--- source/js/replace-cover.js
+++ source/js/replace-cover.js
@@ -1,14 +1,15 @@
+ // 修改默认图片，改成随机图片
 (() => {
     const bgs = [
+        '/images/cover/春.png',
+        '/images/cover/夏.png',
+        '/images/cover/秋.png',
         '/images/cover/冬.png',
         '/images/cover/剑客.png',
-        '/images/cover/夏.png',
-        '/images/cover/山水.png',
-        '/images/cover/春.png',
-        '/images/cover/海岸.png',
         '/images/cover/猫.png',
         '/images/cover/猫2.png',
-        '/images/cover/秋.png',
+        '/images/cover/海岸.png',
+        '/images/cover/山水.png',
     ];
 
     [...document.querySelectorAll('img')].filter(it=>it.src?.endsWith('/images/cover/default.png')).forEach(img => {
